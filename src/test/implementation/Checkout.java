da9c8979edd44e083eb38502315f209ec1963e82
zyt <ijumper@163.com>
2017-05-24 19:45:41 +0800

    enable Scenario Outline

--- src/test/implementation/Checkout.java
+++ src/test/implementation/Checkout.java
@@ -1,13 +1,16 @@
 package implementation;
 
 public class Checkout {
+    private int runningTotal = 0;
+
 
     public void add(int count, int price)  {
+        runningTotal += (count * price);
 
     }
 
     public int total() {
-        return 40;
+        return runningTotal;
     }
 
 
--- src/test/step_definitions/CheckoutSteps.java
+++ src/test/step_definitions/CheckoutSteps.java
@@ -1,10 +1,7 @@
 package step_definitions;
 
 import cucumber.api.java.en.*;
-
 import cucumber.api.PendingException;
-
-
 import static org.junit.Assert.*;
 import implementation.Checkout;
 
@@ -21,7 +18,6 @@ public class CheckoutSteps {
 
     @When("^I checkout (\\d+) \"(.*?)\"$")
     public void iCheckout(int itemCount, String itemName) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         checkout = new Checkout();
         checkout.add(itemCount, bananaPrice);
     }
@@ -29,7 +25,6 @@ public class CheckoutSteps {
 
     @Then("^the total price should be (\\d+)c$")
     public void theTotalPriceShouldBeC(int total) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
         assertEquals(total, checkout.total());
     }
 }
